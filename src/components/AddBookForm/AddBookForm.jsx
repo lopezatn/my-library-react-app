b7fc43aba1dc11bebab2c9e4322552a6d0e1f8eb
lopezatn <agustinlpzb@gmail.com>
2024-01-08 16:58:53 +0100

    refactorization of AddBookForm, testing of AddBookForm

--- src/components/AddBookForm/AddBookForm.jsx
+++ src/components/AddBookForm/AddBookForm.jsx
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddBookForm.css";
 import { useDispatch } from "react-redux";
 import { addBook } from "../../features/books/bookSlice";
 
 function AddBookForm() {
   const dispatch = useDispatch();
+  const [author, setAuthor] = useState("test1");
+  const [title, setTitle] = useState("");
+  const [pages, setPages] = useState(0);
+  const [isRead, setIsRead] = useState(false);
+  const id = Math.floor(Math.random() * 900);
 
   const createBook = (e) => {
     e.preventDefault();
 
-    console.log(e.target)
-    const author = e.target.author?.value;
-    const title = e.target.book?.value;
-    const pages = parseInt(e.target.pages?.value);
-    const isRead = e.target.isRead?.checked;
-    const id = Math.floor(Math.random() * 900);
-
+    console.log({ author, title, pages, isRead, id })
     dispatch(addBook({ author, title, pages, isRead, id }));
 
     e.target.reset();
@@ -26,25 +25,25 @@ function AddBookForm() {
         <div>
           <label htmlFor="author">Author:</label>
           <br />
-          <input type="text" id="author" data-testid="author" />
+          <input type="text" id="author" data-testid="author" onChange={(e) => setAuthor(e.target.value)} />
         </div>
 
         <div>
           <label htmlFor="book">Title:</label>
           <br />
-          <input type="text" id="book" data-testid="title" />
+          <input type="text" id="book" data-testid="title" onChange={(e) => setTitle(e.target.value)}/>
         </div>
 
         <div>
           <label htmlFor="pages">Pages:</label>
           <br />
-          <input type="number" id="pages" data-testid="pages" />
+          <input type="number" id="pages" data-testid="pages" onChange={(e) => setPages(parseInt(e.target.value))}/>
         </div>
 
         <div>
           <label htmlFor="isRead">Read?</label>
           <br />
-          <input type="checkbox" id="isRead" data-testid="read" />
+          <input type="checkbox" id="isRead" data-testid="read" onChange={(e) => setIsRead(e.target.checked)}/>
         </div>
         <button type="submit" data-testid="add-book">Add Book</button>
       </form>
--- src/components/AddBookForm/AddBookForm.test.js
+++ src/components/AddBookForm/AddBookForm.test.js
@@ -3,37 +3,34 @@ import { screen } from "@testing-library/react";
 import { renderWithProviders } from '../../../tests/test-utils';
 import AddBookForm from './AddBookForm';
 import userEvent from '@testing-library/user-event';
+import * as bookSlice from "../../features/books/bookSlice";
 
-it("should invoke the createBook function on form submit", () => {
-    const mockDispatch = jest.fn();
+describe('AddBookForm', () => {
+    it("should invoke the createBook function on form submit", () => {
+        const addBookSpy = jest.spyOn(bookSlice, 'addBook');
     
-    jest.mock("react-redux", () => ({
-        ...jest.requireActual("react-redux"),
-        useDispatch: () => mockDispatch,
-    }));
-
-    renderWithProviders(
-        <AddBookForm/>
-    );
-
-    userEvent.type(screen.getByTestId("author"), "John Doe");
-    expect(screen.getByTestId("author")).toHaveValue("John Doe");
-
-    userEvent.type(screen.getByTestId("title"), "Sample Book");
-    expect(screen.getByTestId("title")).toHaveValue("Sample Book");
-    userEvent.type(screen.getByTestId("pages"), "300");
-    expect(screen.getByTestId("pages")).toHaveValue(300);
-    userEvent.click(screen.getByTestId("read"));
-    expect(screen.getByTestId("read")).toBeChecked();
+        renderWithProviders(
+            <AddBookForm/>
+        );
     
-    userEvent.click(screen.getByTestId("add-book"));
-
-
-    expect(mockDispatch).toHaveBeenCalledWith({
-        author: "John Doe",
-        title: "Sample Book",
-        pages: 300,
-        isRead: true,
-        id: expect.any(Number),
+        userEvent.type(screen.getByTestId("author"), "John Doe");
+        expect(screen.getByTestId("author")).toHaveValue("John Doe");
+        userEvent.type(screen.getByTestId("title"), "Sample Book");
+        expect(screen.getByTestId("title")).toHaveValue("Sample Book");
+        userEvent.type(screen.getByTestId("pages"), "300");
+        expect(screen.getByTestId("pages")).toHaveValue(300);
+        userEvent.click(screen.getByTestId("read"));
+        expect(screen.getByTestId("read")).toBeChecked();
+        
+        userEvent.click(screen.getByTestId("add-book"));
+    
+    
+        expect(addBookSpy).toHaveBeenCalledWith({
+            author: "John Doe",
+            title: "Sample Book",
+            pages: 300,
+            isRead: true,
+            id: expect.any(Number),
+        });
     });
 });
