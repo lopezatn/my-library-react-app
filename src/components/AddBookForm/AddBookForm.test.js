215d3995ccef8710cce17058b1c11afdefe8949f
lopezatn <agustinlpzb@gmail.com>
2024-01-10 11:56:22 +0100

    added testings for BookForm component

--- src/components/AddBookForm/AddBookForm.test.js
+++ src/components/AddBookForm/AddBookForm.test.js
@@ -6,13 +6,17 @@ import userEvent from '@testing-library/user-event';
 import * as bookSlice from "../../features/books/bookSlice";
 
 describe('AddBookForm', () => {
+    beforeEach(() => {
+        jest.restoreAllMocks();
+    });
+
     it("should invoke the createBook function on form submit", () => {
         const addBookSpy = jest.spyOn(bookSlice, 'addBook');
-    
+
         renderWithProviders(
             <AddBookForm/>
-        ); 
-    
+        );
+
         userEvent.type(screen.getByTestId("author"), "John Doe");
         expect(screen.getByTestId("author")).toHaveValue("John Doe");
         userEvent.type(screen.getByTestId("title"), "Sample Book");
@@ -32,6 +36,36 @@ describe('AddBookForm', () => {
             isRead: true,
             id: expect.any(Number),
         });
-    });    
-});
+    });
 
+    it("AddBook Button should be disabled for empty fields", () => {
+    
+        renderWithProviders(
+            <AddBookForm/>
+        );
+
+        userEvent.type(screen.getByTestId("author"), "");
+        userEvent.type(screen.getByTestId("title"), "");
+        userEvent.type(screen.getByTestId("pages"), "");
+        
+        userEvent.click(screen.getByTestId("add-book"));
+        expect(screen.getByTestId("add-book")).toHaveAttribute("disabled");
+        
+    });    
+
+    it("AddBook Button should not be disabled for non-empty fields", () => {
+    
+        renderWithProviders(
+            <AddBookForm/>
+        );
+
+        userEvent.type(screen.getByTestId("author"), "asd");
+        userEvent.type(screen.getByTestId("title"), "asd");
+        userEvent.type(screen.getByTestId("pages"), "123");
+        
+        userEvent.click(screen.getByTestId("add-book"));
+        expect(screen.getByTestId("add-book")).not.toHaveAttribute("disabled");
+        
+    });
+
+});
