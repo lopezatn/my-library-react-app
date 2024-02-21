490c11da9918785efcd7f131937856b293cf99ff
lopezatn <agustinlpzb@gmail.com>
2024-02-21 16:59:20 +0100

    redux initial state from localstorage added

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,6 @@ import bookSlice from './slices/books/bookSlice';
 
 const storedData = localStorage.getItem('bookData');
 const initialState = storedData ? JSON.parse(storedData) : { books: { booksList: [] }};
-console.log('storedData: ', storedData)
-console.log('initialState: ', initialState);
 
 export default configureStore({
   reducer: {
